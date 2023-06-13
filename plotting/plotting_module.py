d1b7141efa7b135f775fbd052ec8ea02ffe0e330
PetrosStefanou <petrouil1@hotmail.com>
2023-06-13 18:58:14 +0200

    Create a function plot_elements() that plots the basic elements of the pulsar magnetosphere & minor cleanups and simplifications.

--- plotting/plotting_module.py
+++ plotting/plotting_module.py
@@ -19,6 +19,9 @@ def set_matplotlib_defaults(SMALL_SIZE = 20, MEDIUM_SIZE = 25, BIGGER_SIZE = 30)
    plt.rcParams['figure.figsize'] = (16,10)
    plt.rcParams["text.latex.preamble"] = [r'\usepackage{xfrac}', r'\usepackage{bm}']
 
+   custom_colors = ['#386cb0', '#f0027f', '#bf5b17', '#666666', '#7fc97f', '#beaed4', '#fdc086', '#ffff99']
+   plt.rcParams['axes.prop_cycle'] = plt.cycler(color=custom_colors)
+
    plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
    plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
    plt.rc('axes', labelsize=BIGGER_SIZE)     # fontsize of the x and y labels
@@ -33,6 +36,33 @@ def set_matplotlib_defaults(SMALL_SIZE = 20, MEDIUM_SIZE = 25, BIGGER_SIZE = 30)
 # ===================================================================================================
 
 
+def plot_elements(ax, X, Z, P, Pc, parameters, surface=True, separatrix=True, lc=True):
+   """
+      Plot the various elements of the pulsar magnetosphere.
+   """
+   
+   # Choose colors
+   color_star = 'lightsteelblue'
+   color_lc = 'gold'
+   color_sep = 'firebrick'
+
+   # Plot surface
+   if surface:
+      ax.add_patch(patches.Circle((0.,0.), 1, color=color_star, zorder=100, label="Star"))
+   
+   # Plot the separatrix
+   if separatrix:
+      ax.contour(X, Z, P, levels=[Pc], linewidths=3, colors=color_sep, zorder=99, labels='Separatrix')
+   
+   # Plot the light cylinder
+   if lc:
+      ax.axvline(parameters['R_lc'], color=color_lc, zorder=98, label='$R_{lc}$')
+
+   return
+
+
+# ===================================================================================================
+
 
 def plot_pinn(X, Z, P, Pc, fb, hb, Np, parameters):
    
@@ -41,11 +71,6 @@ def plot_pinn(X, Z, P, Pc, fb, hb, Np, parameters):
    '''
    
    fig, axes = plt.subplots(2,2, sharex='col', sharey='row')
-   
-   # Properties of surface, separatrix and LC
-   color_star = '#CECFD2'
-   color_lc = 'green'
-   color_sep = 'red'
 
    # Properties of contour lines
    lines_levels = 10
@@ -62,17 +87,8 @@ def plot_pinn(X, Z, P, Pc, fb, hb, Np, parameters):
    # Loop over all quantities to be plotted
    for i, ax in enumerate(axes.ravel()):
       
-      # Set equal aspect ratio in all figures 
-      # ax.set_aspect ('equal')
-
-      # Plot separatrix
-      ax.contour(X, Z, P, levels = [Pc], colors=color_sep, labels='Separatrix')
-
-      # Plot surface
-      ax.add_patch(patches.Circle((0.,0.), 1, color=color_star, zorder=100, label="Star"))
-
-      # Plot LC
-      ax.axvline(parameters['R_lc'], color=color_lc, label='$R_{lc}$')
+      # Plot pulsar magnetosphere elements
+      plot_elements(ax, X, Z, P, Pc, parameters)
 
       # Set plot limits
       ax.set_xlim ([0, 2*parameters['R_lc']])
@@ -141,15 +157,10 @@ def plot_pinn(X, Z, P, Pc, fb, hb, Np, parameters):
 # ===================================================================================================
 
 
-def plot_scalar_functions(X, Z, P, Pc, T, T_prime, parameters):
+def plot_scalar_functions(X, Z, P, Pc, T, Tprime, parameters):
     
    fig, axes = plt.subplots(2,2, sharex='col', sharey='row')
    
-   # Properties of surface, separatrix and LC
-   color_star = '#CECFD2'
-   color_lc = 'green'
-   color_sep = 'red'
-
    # Properties of contour lines
    lines_levels = 10
    lines_color = 'black'
@@ -160,22 +171,13 @@ def plot_scalar_functions(X, Z, P, Pc, T, T_prime, parameters):
    
    # Properties of each individual figure
    titles = ["$P$", "$T$", "$T'$", "$TT'$"]
-   scalars = [P, T, T_prime, T*T_prime]
+   scalars = [P, T, Tprime, T*Tprime]
 
    # Loop over all quantities to be plotted
    for i, ax in enumerate(axes.ravel()):
       
-      # Set equal aspect ratio in all figures 
-      # ax.set_aspect ('equal')
-
-      # Plot separatrix
-      ax.contour(X, Z, P, levels = [Pc], colors=color_sep, labels='Separatrix')
-
-      # Plot surface
-      ax.add_patch(patches.Circle((0.,0.), 1, color=color_star, zorder=100, label="Star"))
-
-      # Plot LC
-      ax.axvline(parameters['R_lc'], color=color_lc, label='$R_{lc}$')
+      # Plot pulsar magnetosphere elements
+      plot_elements(ax, X, Z, P, Pc, parameters)
 
       # Set plot limits
       ax.set_xlim ([0, 2*parameters['R_lc']])
@@ -197,8 +199,8 @@ def plot_scalar_functions(X, Z, P, Pc, T, T_prime, parameters):
       # Exclude axis, infs and nans
       slc = np.s_[:,1:-1]
       f = np.abs(scalars[i][slc])
-      # mask = np.isfinite(np.abs(f))
-      # f = np.where(mask, f, np.nan)
+      mask = np.isfinite(np.abs(f))
+      f = np.where(mask, f, 0)
 
       # Choose which field lines will be plotted (plot only in open region)
       f_lines = np.where(P[slc] <= Pc, f, np.nan)
@@ -238,33 +240,30 @@ def plot_scalar_functions(X, Z, P, Pc, T, T_prime, parameters):
 # ===================================================================================================
 
 
-def plot_fields(X, Z, P, Pc, B_mag, E_mag, div_E, parameters):
+def plot_fields(X, Z, P, Pc, B_mag, E_mag, div_E, div_B, parameters):
     
    fig, axes = plt.subplots(1,2, sharex='col', sharey='row')
 
-   # Exclude axis
-   if parameters ['coordinates'] == 'spherical' or 'compactified':
-      slc = np.s_[:,1:-1]
-   else:
-      slc = np.s_[:,:]
-
    # Set up
+   slc = np.s_[:,1:-1]
+
+
    cmap_levels = 50
-   color_star = '#CECFD2'
-   color_lc = 'green'
-   color_sep = 'red'
+
    color_lines = 'black'
-   cmap = 'coolwarm'
-   titles = ['$B^2 - E^2$', '$\\nabla \cdot \\mathbf{E}$']
-   scalars = [B_mag ** 2 - E_mag ** 2, div_E]
-   linthresh = [1e-9, 1e-6]
+   cmap = 'coolwarm_r'
+   # titles = ['$B^2 - E^2$', '$\\nabla \cdot \\mathbf{E}$']
+   titles = ['$\\nabla \cdot \\mathbf{B}$', '$\\nabla \cdot \\mathbf{E}$']
+
+   # scalars = [B_mag ** 2 - E_mag ** 2, div_E]
+   scalars = [div_B, div_E]
 
    for i, ax in enumerate(axes.ravel()):
-        
-      # ax.set_aspect ('equal')
-      ax.contour(X, Z, P, levels=[Pc], colors=color_sep, labels='Separatrix')
-      ax.add_patch(patches.Circle((0.,0.), 1, color=color_star, zorder=100, label="Star"))
-      ax.axvline(parameters['R_lc'], color=color_lc, label='$R_{lc}$')
+      
+      # Plot pulsar magnetosphere elements
+      plot_elements(ax, X, Z, P, Pc, parameters)
+
+
       ax.set_xlabel('x')
       ax.set_ylabel('z', rotation=0)
       ax.set_xlim ([0, 2*parameters['R_lc']])
@@ -273,16 +272,27 @@ def plot_fields(X, Z, P, Pc, B_mag, E_mag, div_E, parameters):
       ax.set_yticks(np.arange(-15,16,5))
       ax.set_title(titles[i])
 
-      f = scalars[i][slc]
+      f = (scalars[i][slc])
+      mask = np.isfinite(f)
+      min_val = np.floor(np.log10(np.abs(scalars[1][slc])[mask].min()))
+      max_val = np.ceil(np.log10(np.abs(scalars[1][slc])[mask].max()))
+      min_val = -7
+      max_val = 0
+      cmap_levels = np.logspace(min_val, max_val, 50)
+      cmap_levels = np.hstack([-cmap_levels[::-1], cmap_levels])
+                           
       contf = ax.contourf (X[slc], Z[slc]
                            , f
+                           , levels=cmap_levels
+                           , norm=colors.SymLogNorm(linthresh=10**min_val, linscale=1)
+                           , locator=ticker.SymmetricalLogLocator(linthresh=10**min_val, base=10)
                            , cmap=cmap
-                           , norm=colors.SymLogNorm(linthresh=linthresh[i], linscale=1)
-                           , locator=ticker.SymmetricalLogLocator(linthresh=linthresh[i], base=10)
                            )
       
-      cbar = fig.colorbar(contf, ax=ax, format=ticker.LogFormatterMathtext(), shrink=0.585)
-
+      cbar = fig.colorbar(contf, ax=ax, format=ticker.LogFormatterMathtext())
+      cbar_ticks_number = int(np.abs(max_val - min_val + 1))
+      ticks = np.logspace(min_val, max_val, cbar_ticks_number)
+      cbar.set_ticks(np.hstack([-ticks[-1:0:-3], 0, ticks[2::3]]))
    return
 
 
@@ -294,22 +304,20 @@ def plot_pulsar_equation (X, Z, pulsar_eq, P, Pc, parameters):
    fig, ax = plt.subplots()
 
    # Set up
-   slc = np.s_[1:-1,1:-1]
+   slc = np.s_[:,1:-1]
    mask = np.isfinite(pulsar_eq[slc] / P[slc])
    # min_val = np.floor(np.log10(np.abs(pulsar_eq[slc][mask]).min()))
    min_val = -6
-   max_val = np.ceil(np.log10(np.abs(pulsar_eq[slc][mask] / P[slc][mask]).max()))-1
+   max_val = np.ceil(np.log10(np.abs(pulsar_eq[slc][mask]).max()))
    levels = np.logspace(min_val, max_val, 50)
-   color_star = '#CECFD2'
-   color_lc = 'green'
-   color_sep = 'red'
+   
+   
    color_lines = 'black'
    cmap = 'copper'
-        
-   # ax.set_aspect ('equal')
-   # ax.contour(X, Z, P, levels=[Pc], colors=color_sep, labels='Separatrix')
-   ax.add_patch(patches.Circle((0.,0.), 1, color=color_star, zorder=100, label="Star"))
-   # ax.axvline(parameters['R_lc'], color=color_lc, label='$R_{lc}$')
+
+   # Plot pulsar magnetosphere elements
+   plot_elements(ax, X, Z, P, Pc, parameters)
+
    ax.set_xlabel('x')
    ax.set_ylabel('z', rotation=0)
    ax.set_xlim ([0, 2*parameters['R_lc']])
@@ -318,11 +326,11 @@ def plot_pulsar_equation (X, Z, pulsar_eq, P, Pc, parameters):
    ax.set_yticks(np.arange(-15,16,5))
 
    contf = ax.contourf(X[slc], Z[slc]
-                       , np.abs(pulsar_eq[slc] / P[slc])
+                       , np.abs(pulsar_eq[slc])
                        , cmap=cmap
-                       , norm=colors.LogNorm(vmin=10**min_val, vmax=10**max_val)
+                       , norm=colors.LogNorm()
                        , levels=levels
-                       , extend='both'
+                       , extend='min'
                       )
 
    cbar = fig.colorbar(contf)
@@ -335,32 +343,22 @@ def plot_pulsar_equation (X, Z, pulsar_eq, P, Pc, parameters):
 # ===================================================================================================
 
 
-def plot_T_of_P (P, Pc, T, T_prime, parameters):
+def plot_T_of_P (P, Pc, T, Tprime, parameters):
     
-   fig, ax = plt.subplots(1, 1)
+   fig, axes = plt.subplots(1, 2)
 
-   # Mask T_prime to hide divergent values due to numerical derivation
-   mask = np.abs(T_prime) < 0.15
-   T_prime_masked = np.where(mask, T_prime, np.nan)
-   
    #Calculate monopole solution for normalisation and comparison
    # P_mon = 1 / parameters['R_lc'] (1 - )
-   scalars = [-T, -T_prime, T * T_prime]
-   scalars = [-T]
-   # scalars_monopole = [-T, -T_prime_masked, T * T_prime_masked]
+   scalars = [-T, T * Tprime]
+   
+   for i, ax in enumerate(axes):
+      f = scalars[i].ravel()
 
-   # for i, ax in enumerate(axes):
-      # f = scalars[i].ravel()
+      ax.set_xlim([0, 1.25 * Pc])
+      ax.axvline(Pc, c='k', alpha=0.5, ls='--')
 
-   # ax.set_xlim([0, 1.1 * Pc])
-   ax.axvline(Pc, c='k', alpha=0.5, ls='--')
-
-   ax.scatter(P.ravel(), -T, s=10, marker='*')
-   fig, ax = plt.subplots(1, 1)
-
-   ax.axvline(Pc, c='k', alpha=0.5, ls='--')
-
-   ax.scatter(P.ravel(), -T*T_prime, s=10, marker='*')
+      ax.scatter(P.ravel(), f, s=10, marker='*', color='#1b9e77')
+   
 
    return
 
